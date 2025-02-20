e4085bb31506864571506c7b41172cbc734585f1
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-02-20 22:33:42 +0900

    01

---
+++ basic/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main()
+{
+    // 자료형
+    // - 변수가 어떤 자료의 형태로 표현될 것 인지 알려주는 것
+    // 정수형 : char(1) , short(2) , int(4) , long(4) , long long(8)
+    // 실수형 : float(4) , double(8)
+
+
+    int i = 0;
+    unsigned char c = -12; // 0 ~ 255
+    signed char c2 = -12; // -128 ~ 127
+
+    std::cout << c << c2 << std::endl;
+    std::cout << "Test Output" << std::endl;
+    std::cin.get();  // 프로그램 종료 전 대기
+
+
+    return 0;
+}
