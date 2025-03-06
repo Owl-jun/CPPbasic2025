c852f2520ad1b75d306d2a54dbd6088a1fcdd93b
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-03-06 09:07:40 +0900

    00

---
+++ basic/data/data/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <wchar.h>
+
+int my_cmp(const wchar_t* left, const wchar_t* right)
+{
+	// 1 . 인덱스 별로 비교
+	// 2 . 모든게 같다면 0
+	// 3 . 
+	
+
+	return;
+}
+
+
+
+
+int main() {
+
+	my_cmp(L"abc", L"abc");
+	my_cmp(L"abc", L"ab");
+	my_cmp(L"ab", L"abc");
+
+	return 0;
+}
