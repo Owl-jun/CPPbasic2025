0240a10e6f49b1771fc135c0be1d55ce091dc6a9
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-03-07 09:03:37 +0900

    00

--- basic/data/data/test.cpp
+++ basic/data/data/test.cpp
@@ -1,24 +1,57 @@
 #include <iostream>
 #include <wchar.h>
 
-int my_cmp(const wchar_t* left, const wchar_t* right)
-{
-	// 1 . 인덱스 별로 비교
-	// 2 . 모든게 같다면 0
-	// 3 . 
-	
 
-	return;
+// 지역 , 전역 , 정적(static) , 외부(extern)
+
+
+
+
+int leng(const wchar_t* data)
+{
+	static int sInt = 0;
+	int Int = 0;
+	++sInt;
+	++Int;
+	int i = 0;
+	while (true)
+	{
+		if (data[i] != 0) { ++i; }
+		else { break; }
+	}
+	return i;
 }
 
-
+int my_cmp(const wchar_t* left, const wchar_t* right)
+{
+	int a = 5;
+	int l = leng(left);
+	int r = leng(right);
+	int min = l >= r ? r : l;
+	int result = 0;
+	for (int i = 0; i < min; ++i)
+	{
+		if (left[i] == right[i]) { continue; }
+		
+		if (left[i] < right[i]) { return -1; }
+		else if (left[i] > right[i]) { return 1; }
+	}
+	return result;
+}
 
 
 int main() {
 
+	int a = 3;
 	my_cmp(L"abc", L"abc");
 	my_cmp(L"abc", L"ab");
-	my_cmp(L"ab", L"abc");
-
+	my_cmp(L"bc", L"abc");
+	leng(L"ab");
+	leng(L"ab");
+	leng(L"ab");
+	leng(L"ab");
+	leng(L"ab");
+	leng(L"ab");
+	leng(L"ab");
 	return 0;
 }
